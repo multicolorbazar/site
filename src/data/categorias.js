5def15759379661c2aa9cbc660b8099170413345
mcorteze <mcorteze23@gmail.com>
2024-07-26 17:53:27 -0400

    07261753

--- src/data/categorias.js
+++ src/data/categorias.js
@@ -1,34 +1,10 @@
 const categorias = [
-    {
-        id: 1,
-        nombre: 'Ropa',
-        imagen: require('../images/categorias/categoria001.png')
-    },
-    {
-        id: 2,
-        nombre: 'Accesorios',
-        imagen: require('../images/categorias/categoria002.png')
-    },
-    {
-        id: 3,
-        nombre: 'Deporte',
-        imagen: require('../images/categorias/categoria003.png')
-    },
-    {
-        id: 4,
-        nombre: 'Juguetes',
-        imagen: require('../images/categorias/categoria004.png')
-    },
-    {
-        id: 5,
-        nombre: 'Útiles Escolares',
-        imagen: require('../images/categorias/categoria005.png')
-    },
-    {
-        id: 6,
-        nombre: 'Maquillaje',
-        imagen: require('../images/categorias/categoria006.png')
-    }
+    { id: 1, nombre: 'Arte y librería', imagen: require('../images/categorias/categoria001.png') },
+    { id: 2, nombre: 'Cocina', imagen: require('../images/categorias/categoria002.png') },
+    { id: 3, nombre: 'Digital', imagen: require('../images/categorias/categoria003.png') },
+    { id: 4, nombre: 'Educación', imagen: require('../images/categorias/categoria004.png') },
+    { id: 5, nombre: 'Empaque', imagen: require('../images/categorias/categoria005.png') },
+    { id: 6, nombre: 'Hogar', imagen: require('../images/categorias/categoria006.png') },     
 ];
 
 export default categorias;
