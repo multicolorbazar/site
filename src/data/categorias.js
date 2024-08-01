c0bb8ec8e3a9f6ca4a8c789fdea1ea86b1e40817
mcorteze <mcorteze23@gmail.com>
2024-08-01 18:22:09 -0400

    08011822

--- src/data/categorias.js
+++ src/data/categorias.js
@@ -1,11 +1,12 @@
 const categorias = [
-    { id: 1, nombre: 'Arte y librería', imagen: require('../images/categorias/categoria001.png') },
-    { id: 6, nombre: 'Hogar', imagen: require('../images/categorias/categoria006.png') },
+    { id: 1, nombre: 'Librería y papelería', imagen: require('../images/categorias/categoria001.png') },
+    { id: 2, nombre: 'Hogar', imagen: require('../images/categorias/categoria006.png') },
     { id: 3, nombre: 'Digital', imagen: require('../images/categorias/categoria003.png') },
     { id: 4, nombre: 'Educación', imagen: require('../images/categorias/categoria004.png') },
-    { id: 5, nombre: 'Empaque', imagen: require('../images/categorias/categoria005.png') },
-    { id: 6, nombre: 'Alimento', imagen: require('../images/categorias/categoria008.png') },
-      
+    { id: 5, nombre: 'Alimento', imagen: require('../images/categorias/categoria008.png') },
+    { id: 6, nombre: 'Mascotas', imagen: require('../images/categorias/categoria009.png') },
+    { id: 7, nombre: 'Actividades', imagen: require('../images/categorias/categoria010.png') },
+     
 ];
 
 export default categorias;
--- src/data/subcategoriaArticulos.js
+++ src/data/subcategoriaArticulos.js
@@ -2,14 +2,14 @@ const subcategoriaArticulos = [
     { id_subcategoria: 1, nombre: 'Delantal infantil', id_categoria: 1},
     { id_subcategoria: 2, nombre: 'Libro para colorear', id_categoria: 1},
     { id_subcategoria: 3, nombre: 'Plumón', id_categoria: 1},
-    { id_subcategoria: 4, nombre: 'Bolsa de regalo', id_categoria: 5},
-    { id_subcategoria: 5, nombre: 'Cinta adhesiva', id_categoria: 5},
+    { id_subcategoria: 4, nombre: 'Bolsa de regalo', id_categoria: 1},
+    { id_subcategoria: 5, nombre: 'Cinta adhesiva', id_categoria: 1},
     { id_subcategoria: 6, nombre: 'Libro educativo', id_categoria: 4},
     { id_subcategoria: 7, nombre: 'Tabla de cocina', id_categoria: 2},
     { id_subcategoria: 8, nombre: 'Funda de protección', id_categoria: 2},
     { id_subcategoria: 9, nombre: 'Gorro para ducha', id_categoria: 2},
-    { id_subcategoria: 10, nombre: 'Leche vegetal', id_categoria: 6},
-    { id_subcategoria: 11, nombre: 'Cinta de regalo', id_categoria: 5},
+    { id_subcategoria: 10, nombre: 'Leche vegetal', id_categoria: 5},
+    { id_subcategoria: 11, nombre: 'Cinta de regalo', id_categoria: 1},
     { id_subcategoria: 12, nombre: 'Barra de silicona', id_categoria: 1},
     { id_subcategoria: 13, nombre: 'Masas moldeables', id_categoria: 1},
     { id_subcategoria: 14, nombre: 'Stickers', id_categoria: 1},
@@ -17,7 +17,9 @@ const subcategoriaArticulos = [
     { id_subcategoria: 16, nombre: 'Lápices de colores', id_categoria: 1},
     { id_subcategoria: 17, nombre: 'Goma eva', id_categoria: 1},
     { id_subcategoria: 18, nombre: 'Cometa', id_categoria: 1},
-        
+    { id_subcategoria: 19, nombre: 'Bolsa desecho basura', id_categoria: 6},
+    { id_subcategoria: 20, nombre: 'Lápiz grafito', id_categoria: 1},
+      
 ];
 
 export default subcategoriaArticulos;
