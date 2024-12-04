580227899330f70502350f1ad11c39e22b1d1c00
mcorteze <mcorteze23@gmail.com>
2024-12-04 09:22:51 -0300

    12040922

--- src/components/Garage.js
+++ src/components/Garage.js
@@ -6,7 +6,7 @@ import './Garage.css'; // Importa los estilos
 const imagen1 = require('../images/articulos/garage/background_garage_sale.jpg');
 
 const Garage = () => (
-    <Link to="/subcategoria/1" className="garage-container"> {/* Envolvemos el div con un Link */}
+    <Link to="/celebraciones" className="garage-container"> {/* Envolvemos el div con un Link */}
         <div className="garage-banner">
             <img src={imagen1} alt="Imagen de venta de garage" className="fondogaragex1" />
             {/* Rectángulo "Nuevo!" */}
